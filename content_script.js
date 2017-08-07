faa5cded5d0b71d008ad8ce41575ed7c72fe4e27
Willy Hoang <willy.hoang@blueapron.com>
2017-08-07 19:55:41 -0400

    Bare minimum for Chrome extentions
    If a new event popup is loaded on the Google Calendar page, toggling the
    extension will add a new input box for location.

---
+++ content_script.js
@@ -0,0 +1,30 @@
+function addLocationInput() { 
+  var whereHeader = document.createElement("div");
+    whereHeader.setAttribute("class", "color: #1b1b1b; font-family: Arial; font-size: 13px; clear: both;");
+
+    var tileLabel = document.createElement("div");
+    tileLabel.setAttribute("class", "title-label");
+    tileLabel.setAttribute("style","height: 18px; line-height: 18px; color: #9b9b9b; font-size: 11px; font-weight: bold;");
+
+    var label = document.createElement("label");
+    var text = document.createTextNode("Where");
+    label.appendChild(text);
+    tileLabel.appendChild(label);
+
+    whereHeader.appendChild(tileLabel);
+
+    var tileContent = document.createElement("div");
+    tileContent.setAttribute("class", "tile-content");
+    tileContent.setAttribute("style", "height: 36px; line-height: 36px;position: relative;");
+
+    var input = document.createElement("input");
+    tileContent.appendChild(input);
+
+    whereHeader.appendChild(tileContent);
+
+    var elements = document.getElementsByClassName('event-create-container');
+    var element = elements[0];
+    element.insertBefore(whereHeader, element.children[2]);
+}
+
+addLocationInput();
